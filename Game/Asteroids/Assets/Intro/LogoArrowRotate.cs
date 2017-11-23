c922f658a54d21fe8d94b4ec73ba09bea8b535c6
Unknown <guido313@hotmail.nl>
2017-11-23 12:24:16 +0100

    Game intro
    Unity project + splash screen.
    Hoeft niet per se door jullie gebruikt te worden volgende week, want we kunnen dit ook overbrengen. Ik zet het wel hier om zelf op meerdere pcs te kunnen gebruiken.

---
+++ Game/Asteroids/Assets/Intro/LogoArrowRotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoArrowRotate : MonoBehaviour {
+
+    private float currentTime;
+    public float totalTime;
+
+    private Vector3 startingRotation;
+        
+
+	// Use this for initialization
+	void Start () {
+
+        float startTime = Time.time;
+        currentTime = Time.time;
+
+        startingRotation = transform.rotation.eulerAngles;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentTime += Time.deltaTime;
+        if (currentTime <= totalTime)
+        {
+            transform.rotation = Quaternion.Inverse( Quaternion.Euler(new Vector3( startingRotation.x,
+                360f * currentTime / totalTime,
+                startingRotation.z )));
+        }
+
+
+    }
+}
